46bbc4dae3c4a2ecee83158edb97bb0d6850c8f3
Haider694 <khaiderali321.s@gmail.com>
2022-09-04 20:25:56 +0530

    sept4update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const schema= require("./schema/schema")
+const mongoose = require("mongoose");
 const {graphqlHTTP}= require("express-graphql");
 const app=express();
-app.get('/graphql', graphqlHTTP({
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://khaider:<password>@cluster0.kkguppr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("haidata").collection("Author");
+  console.log("error come")
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:true
 }));
